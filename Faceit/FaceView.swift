e2f6fb7d37c52aaa59f867c276c26a04aeac001e
Lejia Duan <lduan@thoughtworks.com>
2017-04-03 16:04:17 -0500

    sets up project

---
+++ Faceit/FaceView.swift
@@ -0,0 +1,18 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by lduan on 4/3/17.
+//  Copyright © 2017 lduan. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+
+}
---
+++ Faceit/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by lduan on 4/3/17.
+//  Copyright © 2017 lduan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
